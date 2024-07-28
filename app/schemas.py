234ad5e2888975ba1218232578e2bce05d4ab592
JeremyMikaleff <jeremy.mikaleff@gmail.com>
2024-07-28 20:28:21 +0200

    feat(api_equide): ajout d'un endpoint pour la pagination et re travail des endpoints pour qu'ils tournent autour de la table chevaux trotteur francais

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,57 @@
 # schemas.py
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List, Any
 
+# ---- Endpoint stats-ifce
+class StatsIfceResponse(BaseModel):
+    dispoIFCE: str
+    lienIfce: str
+    dispoStats: str
+    race: str
+
+
+
+
+# ---- Reponse de base
+class InfosResponse(BaseModel):
+    id: int
+    nom: str
+    sexe: str
+    couleur: str
+    dateDeNaissance: Optional[int] = None
+    naisseur: Optional[str] = None
+    lienIfce: Optional[str] = None
+    pere: str
+    mere: str
+
+    @classmethod
+    def from_orm(cls, obj):
+        return cls(
+            id=obj.id_tf,
+            nom=obj.nom_tf,
+            sexe=obj.sexe_tf,
+            couleur=obj.couleur_tf,
+            dateDeNaissance=obj.annee_naissance_tf,
+            naisseur=obj.naisseur_tf,
+            lienIfce=obj.lien_ifce_tf,
+            pere=obj.pere_tf,
+            mere=obj.mere_tf,
+        )
+
+
+
+# ---- Endpoint pagination
+class PaginationResponse(BaseModel):
+    total_results: int
+    total_pages: int
+    current_page: int
+    page_size: int
+    results: List[InfosResponse]
+
+
+
+
+# ---- Endpoint stat-cheval
 class ChevalResponse(BaseModel):
     nomCheval: str
     nombreCoursesEnregistrer: int
@@ -17,21 +67,9 @@ class ChevalResponse(BaseModel):
     placeMoyenne: float
     montantTotalGagne: int
 
-class StatsIfceResponse(BaseModel):
-    dispoIFCE: str
-    lienIfce: str
-    dispoStats: str
-    race: str
 
-class InfosResponse(BaseModel):
-    nom: str
-    sexe: str
-    couleur: str
-    dateDeNaissance: Optional[int] = None
-    naisseur: Optional[str] = None
-    lienIfce: Optional[str] = None
-    pere: str
-    mere: str
+
+
 
 class GenealogieResponse(BaseModel):
     nom: str
